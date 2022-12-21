0754382e39ba23a09e75e23dee292c7992f95e21
ZAKARIA ELGHANDORI <104373844+ZAKARIA-ELGHANDORI@users.noreply.github.com>
2022-12-21 08:53:56 +0100

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-echo `date` > rebootdate.txt
+echo `date` >> rebootdate.txt
 
 
 ###
-# pour lancer ce script au démarrage de la machine on lance la commande suivant:
+# pour lancer ce script au démarrage de la machine on écrit la commande suivant sur le fichier cron (crontab -e):
 # @reboot path/to/script1.sh
 ####
